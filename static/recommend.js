c8110ca614f5012bc744dc9edb2464265c32cfed
Andrew Molchan <andrew.molchan@siliconmint.com>
2023-06-01 02:33:18 +0300

    fix requirements.py

--- static/recommend.js
+++ static/recommend.js
@@ -12,7 +12,7 @@ $(function() {
   source.addEventListener('input', inputHandler);
 
   $('.movie-button').on('click',function(){
-    var my_api_key = 'YOUR_API_KEY';
+    var my_api_key = '5b0aaa5d807ecd978f14cb5c78d9be64';
     var title = $('.movie').val();
     if (title=="") {
       $('.results').css('display','none');
@@ -26,7 +26,7 @@ $(function() {
 
 // will be invoked when clicking on the recommended movies
 function recommendcard(e){
-  var my_api_key = 'YOUR_API_KEY';
+  var my_api_key = '5b0aaa5d807ecd978f14cb5c78d9be64';
   var title = e.getAttribute('title'); 
   load_details(my_api_key,title);
 }
